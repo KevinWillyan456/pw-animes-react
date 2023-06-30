3f15657def5180390dd9b6b8216733bb7ba54ca7
Kevin Souza <kevinwillyansouza@gmail.com>
2023-06-30 10:50:53 -0400

    Melhorando os estilos da página de anime

--- src/components/AnimeContent.jsx
+++ src/components/AnimeContent.jsx
@@ -3,9 +3,20 @@ import './AnimeContent.css'
 export function AnimeContent({ anime }) {
     return (
         <>
-            <h1 className="title">{anime.nome}</h1>
-            <h2 className="title-synopsis">Sinopse</h2>
-            <div className="synopsis-content">{anime.sinopse}</div>
+            <h1 className="anime-content-title">{anime.nome}</h1>
+            <h2 className="anime-content-title-synopsis">Sinopse</h2>
+            <div className="anime-content-synopsis-content">
+                {anime.sinopse}
+            </div>
+            <div className="anime-content-episode">
+                <div className="current-episode">EP 01</div>
+                <div className="age">
+                    <img
+                        src="https://purchaseway-animes-node.kevinsouza456.repl.co/resources/img/age-12.png"
+                        alt="age"
+                    />
+                </div>
+            </div>
         </>
     )
 }
