4ac4185fa56f8b8a527fa48ef7c2f9f4fb0cb83f
Kevin Souza <kevinwillyansouza@gmail.com>
2024-05-04 09:54:37 -0400

    style: atualizar ano no rodapé para 2024

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ export function Footer() {
     return (
         <>
             <footer className="rodape">
-                <h1 className="title">Pw Animes React - 2023</h1>
+                <h1 className="title">Pw Animes React - 2024</h1>
                 <h2 className="subtitle">Todos os direitos reservados</h2>
             </footer>
         </>
